64b8860d9d3a1e02ebf7e54111f836949dd6e5b3
Paul Long <poloop@gmail.com>
2020-08-06 19:39:28 +0200

    create SalarySubmitForm component

---
+++ src/components/SalarySubmitForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-form class="form">
+    <v-text-field v-show="!submitted" :placeholder="placeholder" v-model="value" min="0" required solo></v-text-field>
+    <v-btn @click="submitHandler">Submit</v-btn>
+  </v-form>
+</template>
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Placeholder'
+    }
+  },
+  data() {
+    return {
+      value: '',
+      submitted: false
+    };
+  },
+  methods: {
+    submitHandler() {
+      if (this.value !== '') {
+        this.submitted = true;
+        this.$emit('submit', parseInt(this.value));
+      }
+    },
+    reset() {
+      this.submitted = false;
+      this.value = '';
+    }
+  }
+};
+</script>
+<style lang="css" scoped>
+.form {
+  min-height: 220px;
+  padding: 50px;
+}
+</style>
